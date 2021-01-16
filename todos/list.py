55fd110439a9f55b473b2526b0bb2a955a394267
Antonio Sanchez <antonio.sanchez734@comunidadunir.net>
2021-01-16 17:40:55 +0000

    Comentario

--- todos/list.py
+++ todos/list.py
@@ -4,7 +4,7 @@ import os
 from todos import decimalencoder
 import boto3
 dynamodb = boto3.resource('dynamodb')
-
+# Commet
 
 def list(event, context):
     table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
